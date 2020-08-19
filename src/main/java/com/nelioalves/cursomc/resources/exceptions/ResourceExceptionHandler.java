ed8afb6be15d4f117985d39ac698b39447404a18
Arthur Oliveira <arthuroliveiradevasconcelos@gmail.com>
2020-08-19 09:41:21 -0500

    Before Heroku

--- src/main/java/com/nelioalves/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/nelioalves/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -4,19 +4,46 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.nelioalves.cursomc.services.exceptions.DataIntegrityException;
 import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 	
 	@ExceptionHandler(ObjectNotFoundException.class)
-	public ResponseEntity<StandardError> resourceNotFound(ObjectNotFoundException e, HttpServletRequest request) {
-		String error = "Object not found";
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
 		HttpStatus status = HttpStatus.NOT_FOUND;
-		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), error, e.getMessage(), request.getRequestURI());
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrityViolation(DataIntegrityException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> MethodArgumentValidation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		ValidationError err = new ValidationError(System.currentTimeMillis(), status.value(), "Erro de validação", request.getRequestURI());
+		for (FieldError x : e.getBindingResult().getFieldErrors()) {
+			err.addError(x.getField(), x.getDefaultMessage());
+		}
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<StandardError> HttpMessageException(HttpMessageNotReadableException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
 
--- src/main/java/com/nelioalves/cursomc/resources/exceptions/StandardError.java
+++ src/main/java/com/nelioalves/cursomc/resources/exceptions/StandardError.java
@@ -7,18 +7,16 @@ public class StandardError implements Serializable {
 	
 	private Long timestamp;
 	private Integer status;
-	private String error;
 	private String message;
 	private String path;
 	
 	public StandardError() {
 	}
 
-	public StandardError(Long timestamp, Integer status, String error, String message, String path) {
+	public StandardError(Long timestamp, Integer status, String message, String path) {
 		super();
 		this.timestamp = timestamp;
 		this.status = status;
-		this.error = error;
 		this.message = message;
 		this.path = path;
 	}
@@ -37,14 +35,6 @@ public class StandardError implements Serializable {
 
 	public void setStatus(Integer status) {
 		this.status = status;
-	}
-
-	public String getError() {
-		return error;
-	}
-
-	public void setError(String error) {
-		this.error = error;
 	}
 
 	public String getMessage() {
